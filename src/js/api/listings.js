c173420efee559fefbc11feac40c4fe9e67ebdbe
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-05-04 16:06:44 +0200

    styling listing cards

--- src/js/api/listings.js
+++ src/js/api/listings.js
@@ -1,10 +1,23 @@
 import { API_BASE_URL } from "./baseurl.js";
 
+const listingContainer = document.querySelector(".listing-container");
+
 async function getListings(url) {
   const response = await fetch(url);
   const json = await response.json();
 
   console.log(json);
+
+  listingContainer.innerHTML = "";
+
+  for (let i = 0; i < json.length; i++) {
+    listingContainer.innerHTML += `
+    <div class="listing-card">
+        <img src="${json[i].media}"/>
+        <h2>${json[i].title}</h2>
+        <p>Current bid: <span class="current-bid">${json[i]._count.bids} credits</span></p>
+    </div>`;
+  }
 }
 
 getListings(`${API_BASE_URL}/api/v1/auction/listings`);
