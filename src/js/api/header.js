07b7ca6686ca614ea6e6f91190c0f7759776a7f7
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-05-20 16:59:22 +0200

    move nav link to new file

---
+++ src/js/api/header.js
@@ -0,0 +1,28 @@
+import { token, userName } from "./storage.mjs";
+import { API_BASE_URL } from "./baseurl.js";
+
+const profileLink = document.querySelector(".profile-link");
+
+async function getProfileLink(url) {
+  try {
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await fetch(url, fetchOptions);
+    const json = await response.json();
+    const avatar = json.avatar
+      ? `<img class="me-3" src="${json.avatar}"/>`
+      : '<div class="me-3 no-avatar"><i class="fa-solid fa-user"></i></div>';
+    profileLink.innerHTML = `
+    <a class="my-profile-link"href="my-profile.html">${avatar} <div>${json.name}</div></a>
+    `;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getProfileLink(`${API_BASE_URL}/api/v1/auction/profiles/${userName}`);
